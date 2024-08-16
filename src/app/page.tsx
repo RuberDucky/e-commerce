664d6cf41d826e87721baa5c3ba0f1d844e4a7b9
jhon <91237764+RuberDucky@users.noreply.github.com>
2024-08-16 18:03:45 +0500

    feat: Add HeroSection component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Header } from "@/sections/Header";
+import HeroSection from "@/sections/Hero";
 
 
 export default function Home() {
   return( <>
   <Header/>
+  <HeroSection/>
   </>);
 }
